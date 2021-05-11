b8f2a2892bf3ded140824238d492838af5451992
kykpi <kylee.burgess.14@gmail.com>
2021-05-11 08:10:43 -0700

    move json conversions to class methods

--- source/group.py
+++ source/group.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 
 
@@ -16,6 +18,25 @@ class Group:
     color: str
     member_id: str
 
+    @classmethod
+    def json_to_class(cls, user_group, group_json) -> Group:
+        group = cls(
+            converted_to_currency=group_json['ownerColor'],
+            invite_link=group_json['inviteLink'],
+            invite_link_active=group_json['inviteLinkActive'],
+            invite_link_hash=group_json['inviteLinkHash'],
+            last_changed=group_json['lastChanged'],
+            minimize_debts=group_json['minimizeDebts'],
+            name=group_json['name'],
+            owner_color=group_json['ownerColor'],
+            group_id=user_group.group_id,
+            order=user_group.order,
+            color=user_group.color,
+            member_id=user_group.member_id,
+        )
+        return group
+
+
 @dataclass
 class UserGroupView:
     group_id: str
@@ -23,5 +44,11 @@ class UserGroupView:
     color: str
     member_id: str
 
-
-
+    @classmethod
+    def from_dict(cls, group_id, group_view_dict) -> UserGroupView:
+        return cls(
+            group_id=group_id,
+            order=group_view_dict['order'],
+            color=group_view_dict['color'],
+            member_id=group_view_dict['member'],
+        )
--- source/settleup.py
+++ source/settleup.py
@@ -1,22 +1,20 @@
+from dataclasses import asdict
+
 from group import Group, UserGroupView
 import pyrebase
 import requests
 import json
 import user_auth_config
 from typing import List
+from user import Config
 
 
 class SettleUp:
 
-    def __init__(self):
-        config = {
-            "apiKey": "AIzaSyCfMEZut1bOgu9d1NHrJiZ7ruRdzfKEHbk",
-            "authDomain": "settle-up-sandbox.firebaseapp.com",
-            "databaseURL": "https://settle-up-sandbox.firebaseio.com",
-            "storageBucket": "settle-up-sandbox.appspot.com"
-        }
+    def __init__(self, config: Config):
+        self.config = config
 
-        firebase = pyrebase.initialize_app(config)
+        firebase = pyrebase.initialize_app(asdict(self.config))
 
         auth = firebase.auth()
         self.user = auth.sign_in_with_email_and_password(user_auth_config.email, user_auth_config.password)
@@ -27,18 +25,28 @@ class SettleUp:
         self.user['idToken']
 
         self.payload = {'auth': self.user['idToken']}
+        # self.config.firebase_url = "https://settle-up-sandbox.firebaseio.com"
+        self.firebase_url = config.databaseURL
+
+    def request(self, data_request) -> dict:
+        user_id = self.user['userId']
+        payload = self.payload
+        firebase_url = self.config.databaseURL
+        r = requests.get(f'{firebase_url}/{data_request}/{user_id}.json',
+                         params=payload)
+        return json.loads(r.text)
 
     def get_groups(self) -> List[Group]:
         # usergroup_json = api.request("userGroups")
-        r = requests.get('https://settle-up-sandbox.firebaseio.com/userGroups/' + str(self.user['userId'] + '.json'),
+        user_id = self.user['userId']
+        r = requests.get(f'https://settle-up-sandbox.firebaseio.com/userGroups/{user_id}.json',
                          params=self.payload)
         usergroup_json = json.loads(r.text)
 
-        # TODO: Move json to class conversion to dataclass method
         user_groups = []
-        for group_id, group in usergroup_json.items():
-            user_group_view = UserGroupView(group_id, group['order'], group['color'], group['member'])
-            user_groups.append(user_group_view)
+        for group_id, usergroup_json in usergroup_json.items():
+            user_group = UserGroupView.from_dict(group_id, usergroup_json)
+            user_groups.append(user_group)
 
         groups = []
         for user_group in user_groups:
@@ -46,28 +54,25 @@ class SettleUp:
 
             r = requests.get('https://settle-up-sandbox.firebaseio.com/groups/' + str(user_group.group_id + '.json'),
                              params=self.payload)
+            # f"Hello, {name}. You are {age}."
+            # convert to fstring
+            r = requests.get(f'https://settle-up-sandbox.firebaseio.com/groups/{user_group.group_id}.json',
+                             params=self.payload)
             group_json = json.loads(r.text)
 
-            # TODO: Move json to class conversion to dataclass method
-            group = group_json
-            group = Group(
-                converted_to_currency=group['ownerColor'],
-                invite_link=group['inviteLink'],
-                invite_link_active=group['inviteLinkActive'],
-                invite_link_hash=group['inviteLinkHash'],
-                last_changed=group['lastChanged'],
-                minimize_debts=group['minimizeDebts'],
-                name=group['name'],
-                owner_color=group['ownerColor'],
-                group_id=user_group.group_id,
-                order=user_group.order,
-                color=user_group.color,
-                member_id=user_group.member_id,
-            )
+            group = Group.json_to_class(user_group, group_json)
             groups.append(group)
         return groups
 
-settleup = SettleUp()
+
+config = Config(
+    apiKey="AIzaSyCfMEZut1bOgu9d1NHrJiZ7ruRdzfKEHbk",
+    authDomain="settle-up-sandbox.firebaseapp.com",
+    databaseURL="https://settle-up-sandbox.firebaseio.com",
+    storageBucket="settle-up-sandbox.appspot.com"
+)
+
+settleup = SettleUp(config)
 groups = settleup.get_groups()
 for group in groups:
     print(group)
